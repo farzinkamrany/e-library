6cba7845c2d78ae1ef33a68f6ef3847a7f4e5adc
farzin <farzink25@gmail.com>
2025-08-17 22:41:07 +0330

    add fonts

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import './fonts/BNazanin.ttf'
+import './fonts/BArshia.ttf'
+
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
